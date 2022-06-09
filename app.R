6841d247f1f65593272617359121f3f9cb15326d
Matt Lampl <lampl.matt@gmail.com>
2022-06-09 15:57:31 -0400

    Graphical Update
    - Adjusted Layout to single column view
    - Added a basemap selector
    - Added 3 new graphs (QQ Plot, Distribution by Year, and Mean HFPA by Year)

--- app.R
+++ app.R
@@ -11,28 +11,45 @@ ui <- fluidPage(
   titlePanel(title = 'HFPA'),
   fluidRow(
     column(width = 3,
-           selectInput(inputId = 'yearSelect', label = 'Select Year', choices = last.10.years),
-           selectInput(inputId = 'varSelect', label = 'Select Metric for Map and Trend Graph', 
-                       choices = c('HFPA', 'Availability', 'Access', 'Utilization')),
-           sliderInput(inputId = 'alphaSlider', 
-                       label = 'Adjust Map Transparency', min = 0, max = 1, value = .8),
-           selectizeInput(inputId = 'tractSelect', label = 'Select Tracts for Trend Graph', 
-                          choices = unique(HFPA.data$GEOID), multiple = T, selected = '42003191700')
            ), # column
     
-    column(width = 8,
+    column(width = 10, offset = 1,
            tabsetPanel(
              tabPanel('Map',
                       h1(textOutput(outputId = 'mapTitle')),
-                      #plotlyOutput(outputId = 'mapPlot', height = '900px'),
-                      tmapOutput(outputId = 'tmapPlot', height = '800px')),
-             tabPanel('Trends',
-                      checkboxInput(inputId = 'trendlineCheckbox', label = 'Show Trendline', value = T),
-                      plotlyOutput(outputId = 'comparePlot', height = '600px'),
-                      verbatimTextOutput(outputId = 'regressionResults')),
+                      fluidRow(
+                        column(width = 2, selectInput(inputId = 'basemap', label = 'Basemap:', 
+                                                      choices = c('Esri.WorldTopoMap', 'OpenStreetMap', 'CartoDB'))),
+                        column(width = 2, selectInput(inputId = 'yearSelect', label = 'Select Year', choices = last.10.years)), # select year
+                        column(width = 2, selectInput(inputId = 'varSelect', label = 'Select Metric', 
+                                                      choices = c('HFPA', 'Availability', 'Access', 'Utilization'))), # select metric
+                        column(width = 3, sliderInput(inputId = 'alphaSlider', 
+                                                      label = 'Adjust Map Transparency', min = 0, max = 1, value = .8)) # transparency slider
+                      ), # fluidRow
+                      tmapOutput(outputId = 'tmapPlot', height = '800px')), # Map
              tabPanel('Raw Data',
+                      h1(textOutput(outputId = 'rawDataTitle')),
                       downloadButton(outputId = 'downloadData', label = 'Download'),
-                      dataTableOutput(outputId = 'yearTable'))
+                      br(),
+                      br(),
+                      dataTableOutput(outputId = 'yearTable')), # Raw Data
+             tabPanel('Trends',
+                      fluidRow(
+                        column(width = 3, selectizeInput(inputId = 'tractSelect', label = 'Select Tracts for Trend Graph', 
+                                                         choices = unique(HFPA.data$GEOID), multiple = T, selected = '42003191700')), # tract select
+                        column(width = 2, 
+                               checkboxInput(inputId = 'trendlineCheckbox', label = 'Show Trendline', value = T), # trendline box
+                               checkboxInput(inputId = 'seCheckbox', label = 'Show Standard Errors', value = F)) # se box
+                        ),
+                      plotlyOutput(outputId = 'comparePlot', height = '600px'),
+                      br(),
+                      verbatimTextOutput(outputId = 'regressionResults'),
+                      plotlyOutput(outputId = 'rankedPlot', height = '600px'),
+                      fluidRow(
+                        column(width = 6, plotOutput(outputId = 'distPlot')),
+                        column(width = 6, plotOutput(outputId = 'meanPlot'))
+                        ),
+                      ), # Trends
              ), #tabsetPanel
            ), # column
   ) # fluidRow
@@ -44,6 +61,8 @@ server <- function(input, output, session) {
   tracts = reactive({input$tractSelect})
   map.alpha = reactive({input$alphaSlider})
   show.trendline = reactive({input$trendlineCheckbox})
+  show.se = reactive({input$seCheckbox})
+  basemap = reactive({input$basemap})
   
   data.table = reactive({
     HFPA.data %>%
@@ -58,7 +77,7 @@ server <- function(input, output, session) {
   
   output$tmapPlot = renderTmap({
     tm_shape(filter(HFPA.data, year == year())) +
-      tm_basemap("Esri.WorldTopoMap") +
+      tm_basemap(basemap()) +
       tm_polygons(metric(), alpha = map.alpha(), palette = 'BuPu')
   }) # render Map
   
@@ -75,9 +94,11 @@ server <- function(input, output, session) {
       ggplotly(p)
     }
     else {
-      p = p + geom_smooth(size = 1, method = 'lm', se = F)
+      p = p + geom_smooth(size = 1, method = 'lm', se = show.se())
       ggplotly(p)
     }
+    
+    
   }) # comparePlot
   
   output$regressionResults = renderText({
@@ -85,15 +106,61 @@ server <- function(input, output, session) {
     for (tract in tracts()) {
       model = lm(data = filter(HFPA.data, GEOID == tract), formula = paste(metric(), '~ year'))
       year.coef = round(model$coefficients['year'], 2)
-      year.pval = round(summary(model)$coef[8], 2)
-      regression.output = paste('Tract', tract, 
-                                'is associated with a', year.coef, 'change in', 
-                                metric(), 'for each additional year (p-val:)', year.pval)
+      year.pval = round(summary(model)$coef[8], 3)
+      
+      # Find if it is significant
+      if (year.pval <= 0.05) {
+        sig.code = 'Significant!'
+      }
+      else {sig.code = ''}
+      
+      regression.output = paste('Tract ', tract, 
+                                ': \tBeta Coef = ', year.coef,
+                                '\tp-value = ', year.pval, 
+                                '\t ', sig.code, sep = '')
       #print(tract)
       output = paste(output, regression.output, '\n')
     }
     output
   }) # Regression Results
+  
+  output$rankedPlot = renderPlotly({
+    p = ggplot(data = HFPA.data,
+               mapping = aes(sample = .data[[input$varSelect]])) +
+      stat_qq(geom = 'point', distribution = 'qunif') +
+      facet_grid(cols = vars(year)) +
+      labs(title = 'Quantile Plot by Year', x = '', y = metric()) +
+      theme_bw() +
+      theme(strip.background = element_rect(fill = 'gold1')) +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = -1))
+    ggplotly(p)
+  })
+  
+  output$distPlot = renderPlot({
+    p = ggplot(data = HFPA.data, mapping = aes(x = HFPA, color = as.factor(year))) +
+      geom_density() +
+      labs(title = paste(metric(), 'Distribution by Year'), x = metric(), y = 'Density') +
+      theme_minimal()
+    p
+  })
+  
+  output$meanPlot = renderPlot({
+    yearly.mean = HFPA.data %>%
+      group_by(year) %>%
+      summarize(mean = mean(HFPA, na.rm = T))
+    
+    p = ggplot(data = yearly.mean, mapping = aes(x = year, y = mean)) +
+      geom_line(color = 'darkblue') +
+      geom_point(size = 3) +
+      labs(title = 'Mean HFPA by Year', x = 'Year', y = 'HFPA') +
+      ylim(0, 40) +
+      theme_minimal()
+    p
+  })
+  
+  output$rawDataTitle = renderText({
+    paste('Raw Data for', year())
+  })
   
   output$yearTable = renderDataTable({
     data.table()
